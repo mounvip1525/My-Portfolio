d812fbf79207505b2cbc68dcdde575a07cd611f0
mounvip1525 <mounvip1525@gmail.com>
2021-03-14 11:11:43 +0530

    initial commit

---
+++ src/ContactMe/ContactMe.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ContactMe extends Component {
+    render() {
+        return (
+            <div>
+                Contact me
+            </div>
+        )
+    }
+}
