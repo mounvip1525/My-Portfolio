74df83c1caee4f89be731f44e8d1a53555f79e6a
mounvip1525 <mounvip1525@gmail.com>
2021-03-15 19:03:21 +0530

    contact page design chnage

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -40,29 +40,38 @@ export default class ContactMe extends Component {
                     <div className={styles.right}>
                         <div className={styles.rightContainer}>
                             <div>
-                            <div>
-                                <label for='fname'>First Name</label>
-                                <input type="text" name='fname' placeholder='John' />
-                            </div>
-                            <div>
-                                <label for='fname'>Last Name</label>
-                                <input type="text" name='lname' placeholder='Doe' />
-                            </div>
-                            <div>
-                                <label for='email'>Email Address</label>
-                                <input type="email" name='email' placeholder='example@gmail.com' />
-                            </div>
-                            <div>
-                                <label for='phone'>Phone Number</label>
-                                <input type="phone" name='phone' placeholder='+91 XXXXXXXXXX' />
-                            </div>
+                                <div>
+                                    <label for='fname'>Your Name</label>
+                                    <input type="text" name='name' placeholder='John Doe' />
+                                </div>
+                                <div>
+                                    <label for='email'>Email Address</label>
+                                    <input type="email" name='email' placeholder='example@gmail.com' />
+                                </div>
+                                <div>
+                                    <label for='phone'>Phone Number</label>
+                                    <input type="phone" name='phone' placeholder='+91 XXXXXXXXXX' />
+                                </div>
                             </div>
                             <div className={styles.message}>
                                 <label for='message'><p>Message</p></label>
-                                <input type="text" name='message' placeholder='Your message' />
+                                <textarea type="text" name='message' placeholder='Your message' />
                             </div>
-                            <button className={styles.send}>Send<SendIcon /></button>
                         </div>
+                        <p>It'd be great if you can tell why you are reaching out to me!</p>
+                            <div className={styles.query}>
+                                <div>
+                                    <label for='c1'><input type="radio" name='c' id='c1'/>Feedback</label>
+                                </div>
+                                <div>
+                                    <label for='c2'> <input type="radio" name='c' id='c2'/>Queries</label>
+                                </div><div>
+                                    <label for='c3'><input type="radio" name='c' id='c3'/>Hiring Purposes</label>
+                                </div><div>
+                                    <label for='c4'><input type="radio" name='c' id='c3'/>Others</label>
+                                </div>
+                            </div>
+                        <button className={styles.send}>Send<SendIcon /></button>
                     </div>
                 </div>
                 </div>
