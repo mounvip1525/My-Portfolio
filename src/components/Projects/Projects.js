ce1e6abdc2c944410d17e246eb81b9c529d8807d
mounvip1525 <mounvip1525@gmail.com>
2021-03-17 16:23:05 +0530

    project setion

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styles from './Projects.module.css';
 import LaunchIcon from '@material-ui/icons/Launch';
 import pic from '../../assests/project1.png';
+import Title from '../Title/Title';
 
 export default class Projects extends Component {
     state={
@@ -9,19 +10,22 @@ export default class Projects extends Component {
     }
     render() {
         return (
-            <div className={styles.container}>
-                {this.state.names.map(name=>{
+            <React.Fragment>
+                <Title name="My Projects" />
+                <div className={styles.container}>
+                    {this.state.names.map(name=>{
                     return(
                         <div className={styles.project}>
                             <img src={pic} alt="My project" />
-                            <div>
+                            <div className={styles.projectName}>
                                 <p>Infinity Gallery</p>
-                                <LaunchIcon />
+                                <div title="Launch Live"><LaunchIcon /></div>
                             </div>
                         </div>
                     )
                 })}
             </div>
+            </React.Fragment>
         )
     }
 }
