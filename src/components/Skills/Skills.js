aacaa75e98d998a1ab6e046f02bdcae2ffda6282
mounvip1525 <mounvip1525@gmail.com>
2021-03-17 19:48:51 +0530

    slider fixes

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import styles from './Skills.module.css';
 
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, DotGroup } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+
 import html from '../../assests/html.svg';
 import javascript from '../../assests/js.svg';
 import react from '../../assests/react.svg';
@@ -16,59 +19,90 @@ import Stars from '../Stars/Stars';
 export default class Skills extends Component {
     render() {
         return (
-            <div className={styles.wrapper}>
-                <Title name="My Skills" />
-                <div className={styles.container}>
-                <div>
-                    <img src={html} alt="HTML" />
-                    <h5>HTML</h5>
-                    <p>My most typed characters: &lt; / &gt;</p>
-                    <Stars count='4'/>
-                </div>
-                <div>
+            <div>
+              <Title name="My Skillsets" />
+              <div className={styles.container}>
+              <CarouselProvider
+                naturalSlideWidth={100}
+                naturalSlideHeight={125}
+                totalSlides={8}
+                isPlaying={true}
+                visibleSlides={4}
+                disableActiveDots={false}
+                interval={3000}>
+                <Slider className={styles.slider}>
+                  <Slide index={0}>
+                  <div className={styles.slideComponent}>
+                        <img src={html} alt="HTML" />
+                        <h5>HTML</h5>
+                        <p>My most typed characters: &lt; / &gt;</p>
+                        <Stars count='4'/>
+                  </div>
+                  </Slide>
+                  <Slide index={1}>
+                    <div className={styles.slideComponent}>
                     <img src={javascript} alt="JavaScript" />
-                    <h5>Javascript</h5>
-                    <p>My Full-Stack euphoria</p>
-                    <Stars count='4' />
+                        <h5>Javascript</h5>
+                        <p>My Full-Stack euphoria</p>
+                        <Stars count='4' />
+                    </div>
+                  </Slide>
+                  <Slide index={2}>
+                    <div className={styles.slideComponent}>
+                        <img src={react} alt="React" />
+                        <h5>React</h5>
+                        <p>I love React like no other thing</p>
+                        <Stars count='3' />
+                    </div>
+                  </Slide>
+                  <Slide index={3}>
+                    <div className={styles.slideComponent}>
+                        <img src={nodejs} alt="NodeJS" />
+                        <h5>NodeJS</h5>
+                        <p>My backend coding utility</p>
+                        <Stars count='2' />
+                    </div>
+                  </Slide>
+                  <Slide index={4}>
+                    <div className={styles.slideComponent}>
+                        <img src={python} alt="Python" />
+                        <h5>Python</h5>
+                        <p>PS: More complicated than looks</p>
+                        <Stars count='3' />
+                    </div>
+                  </Slide>
+                  <Slide index={5}>
+                    <div className={styles.slideComponent}>
+                        <img src={java} alt="Java" />
+                        <h5>Java</h5>
+                        <p>Learning since 10th grade</p>
+                        <Stars count='4' />
+                    </div>
+                  </Slide>
+                  <Slide index={6}>
+                    <div className={styles.slideComponent}>
+                        <img src={mysql} alt="MySQL" />
+                        <h5>MySQL</h5>
+                        <p>Favourites: Databases & MySQL</p>
+                        <Stars count='4' />
+                    </div>
+                  </Slide>
+                  <Slide index={7}>
+                    <div className={styles.slideComponent}>
+                        <img src={git} alt="Git and GitHub" />
+                        <h5>Git and GitHub</h5>
+                        <p>My one stop for Version Control</p>
+                        <Stars count='4' />
+                    </div>
+                  </Slide>
+                </Slider>
+                <div className={styles.buttonContainer}>
+                <ButtonBack>&lt;</ButtonBack>
+                <ButtonNext>&gt;</ButtonNext>
                 </div>
-                <div>
-                    <img src={react} alt="React" />
-                    <h5>React</h5>
-                    <p>I love React like no other thing</p>
-                    <Stars count='3' />
-                </div>
-                <div>
-                    <img src={nodejs} alt="NodeJS" />
-                    <h5>NodeJS</h5>
-                    <p>My backend coding utility</p>
-                    <Stars count='2' />
-                </div>
-                <div>
-                    <img src={python} alt="Python" />
-                    <h5>Python</h5>
-                    <p>PS: More complicated than looks</p>
-                    <Stars count='3' />
-                </div>
-                <div>
-                    <img src={java} alt="Java" />
-                    <h5>Java</h5>
-                    <p>Learning since 10th grade</p>
-                    <Stars count='4' />
-                </div>
-                <div>
-                    <img src={mysql} alt="MySQL" />
-                    <h5>MySQL</h5>
-                    <p>Favourites: Databases & MySQL</p>
-                    <Stars count='4' />
-                </div>
-                <div>
-                    <img src={git} alt="Git and GitHub" />
-                    <h5>Git and GitHub</h5>
-                    <p>My one stop for Version Control</p>
-                    <Stars count='4' />
-                </div>
-            </div>
-            </div>
+              </CarouselProvider>
+              </div>
+              </div>
         )
     }
 }
